963e89e9a59977317c15685f151ea96697f97c0e
KingJin-web <1261337209@qq.com>
2022-01-02 22:05:29 +0800

    ✔ 390. 消除游戏

---
+++ src/main/java/com/king/year_2022/M01/Test2.java
@@ -0,0 +1,19 @@
+﻿package com.king.year_2022.M01;
+
+/**
+ * @program: leetcode_diary
+ * @description: 390. 消除游戏
+ * @author: King
+ * @create: 2022-01-02 21:29
+ */
+public class Test2 {
+    //执行用时： 2 ms , 在所有 Java 提交中击败了 85.24% 的用户 内存消耗： 37.3 MB ,
+    //在所有 Java 提交中击败了 81.43% 的用户
+    public int lastRemaining(int n) {
+        return n == 1 ? 1 : 2 * (n / 2 + 1 - lastRemaining(n / 2));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
