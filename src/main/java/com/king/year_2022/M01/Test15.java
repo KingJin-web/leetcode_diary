4afb66f5d97eef66025ab425f4539cd1b174da0c
KingJin-web <1261337209@qq.com>
2022-01-18 23:24:36 +0800

    ✔ 539. 最小时间差

--- src/main/java/com/king/year_2022/M01/Test15.java
+++ src/main/java/com/king/year_2022/M01/Test15.java
@@ -1,5 +1,8 @@
 package com.king.year_2022.M01;
 
+import com.king.util.Helper;
+import com.king.util.MyPrint;
+
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
@@ -7,12 +10,15 @@ import java.util.List;
 /**
  * @program: leetcode_diary
  * @description: 539. 最小时间差
+ * https://leetcode-cn.com/problems/minimum-time-difference/
  * @author: King
  * @create: 2022-01-16 10:02
  */
 public class Test15 {
     public static void main(String[] args) {
-
+        Test15 test15 = new Test15();
+        MyPrint.print(test15.findMinDifference1(Helper.getList("00:00", "23:59", "00:02", "12:12")));
+        MyPrint.print(test15.findMinDifference2(Helper.getList("00:00", "23:59", "00:02", "12:12")));
 
     }
 
@@ -21,22 +27,39 @@ public class Test15 {
     // 输入：timePoints = ["00:00","23:59","00:00"]
 
     //执行用时： 3 ms , 在所有 Java 提交中击败了 77.73% 的用户 内存消耗： 37.9 MB , 在所有 Java 提交中击败了 89.35% 的用户
-    public int findMinDifference(List<String> timePoints) {
+    public int findMinDifference1(List<String> timePoints) {
         int n = timePoints.size();
         if (n > 1440) {
             return 0;
         }
-        int [] times = new int[n];
-        for (int i = 0 ;i<n;++i){
+        int[] times = new int[n];
+        for (int i = 0; i < n; ++i) {
             String[] s = timePoints.get(i).split(":");
             times[i] = Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);
         }
         Arrays.sort(times);
         int min = Integer.MAX_VALUE;
-        for (int i = 1 ;i<n;++i){
-            min = Math.min(min,times[i] - times[i-1] );
+        for (int i = 1; i < n; ++i) {
+            min = Math.min(min, times[i] - times[i - 1]);
         }
         return Math.min(min, times[0] + 1440 - times[times.length - 1]);
+    }
+
+    public int findMinDifference2(List<String> timePoints) {
+        if (timePoints.size() > 1440) {
+            return 0;
+        }
+        int[] arr = new int[timePoints.size()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(timePoints.get(i).substring(0, 2)) * 60 +
+                    Integer.parseInt(timePoints.get(i).substring(3));
+        }
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < arr.length; i++) {
+            min = Math.min(min, arr[i] - arr[i - 1]);
+        }
+        return Math.min(min, arr[0] + 1440 - arr[arr.length - 1]);
     }
 
     public static void run() {
