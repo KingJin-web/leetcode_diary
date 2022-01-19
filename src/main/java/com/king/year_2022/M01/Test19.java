6b7956e2f6c9d5a1b11145a61bbae761f601773d
KingJin-web <1261337209@qq.com>
2022-01-19 23:05:00 +0800

    ✔ 219. 存在重复元素 II

---
+++ src/main/java/com/king/year_2022/M01/Test19.java
@@ -0,0 +1,19 @@
+package com.king.year_2022.M01;
+
+/**
+ * @program: leetcode_diary
+ * @description: 219. 存在重复元素 II
+ * @author: King
+ * @create: 2022-01-19 21:53
+ */
+public class Test19 {
+
+    //给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j]
+    // 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
+    //
+    //
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+    }
+
+}
