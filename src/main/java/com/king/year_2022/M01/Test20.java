5d03637b46669f6de28e2a3b5785fd0b3602e3ff
KingJin-web <1261337209@qq.com>
2022-01-22 23:06:11 +0800

    ✔ 1332. 删除回文子序列

--- src/main/java/com/king/year_2022/M01/Test20.java
+++ src/main/java/com/king/year_2022/M01/Test20.java
@@ -1,5 +1,7 @@
 package com.king.year_2022.M01;
 
+import java.util.Arrays;
+
 /**
  * @program: leetcode_diary
  * @description: 2029. 石子游戏 IX
@@ -7,4 +9,8 @@ package com.king.year_2022.M01;
  * @create: 2022-01-20 23:23
  */
 public class Test20 {
+    public static void main(String[] args) {
+        int Array;
+
+    }
 }
---
+++ src/main/java/com/king/year_2022/M01/Test22.java
@@ -0,0 +1,23 @@
+package com.king.year_2022.M01;
+
+/**
+ * @program: leetcode_diary
+ * @description: 1332. 删除回文子序列
+ * https://leetcode-cn.com/problems/remove-palindromic-subsequences/
+ * @author: King
+ * @create: 2022-01-22 22:50
+ */
+public class Test22 {
+    //给你一个字符串 s，它仅由字母 'a' 和 'b' 组成。每一次删除操作都可以从 s 中删除一个回文 子序列。
+    //
+    //返回删除给定字符串中所有字符（字符串为空）的最小删除次数。
+    //
+    //「子序列」定义：如果一个字符串可以通过删除原字符串某些字符而不改变原字符顺序得到，那么这个字符串就是原字符串的一个子序列。
+    //
+    //「回文」定义：如果一个字符串向后和向前读是一致的，那么这个字符串就是一个回文。
+    //
+    //
+    public int removePalindromeSub(String s) {
+
+    }
+}
