a475a79d36815b25ee3b0ed534e569132c5cc299
king <1261337209@qq.com>
2022-01-25 23:50:41 +0800

    ✔ 2034. 股票价格波动

---
+++ src/main/java/com/king/year_2022/M01/Test24.java
@@ -0,0 +1,25 @@
+package com.king.year_2022.M01;
+
+/**
+ * @program: leetcode_diary
+ * @description: 1688. 比赛中的配对次数
+ * @author: King
+ * @create: 2022-01-25 23:47
+ */
+public class Test24 {
+    //给你一个整数 n ，表示比赛中的队伍数。比赛遵循一种独特的赛制：
+    //
+    //如果当前队伍数是 偶数 ，那么每支队伍都会与另一支队伍配对。总共进行 n / 2 场比赛，且产生 n / 2 支队伍进入下一轮。
+    //如果当前队伍数为 奇数 ，那么将会随机轮空并晋级一支队伍，其余的队伍配对。总共进行 (n - 1) / 2 场比赛，且产生 (n - 1) / 2 + 1 支队伍进入下一轮。
+    //返回在比赛中进行的配对次数，直到决出获胜队伍为止。
+    //
+    //
+
+    public int numberOfMatches(int n) {
+        return n-1;
+    }
+
+    public static void main(String[] args) {
+        Thread
+    }
+}
