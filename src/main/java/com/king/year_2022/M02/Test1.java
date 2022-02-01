0aa4a2cac78a20dc0d8a969b12000ccc757750b5
king <1261337209@qq.com>
2022-02-01 23:40:25 +0800

    ✔ 1763. 最长的美好子字符串

--- src/main/java/com/king/year_2022/M02/Test1.java
+++ src/main/java/com/king/year_2022/M02/Test1.java
@@ -14,5 +14,27 @@ public class Test1 {
     // 请你返回一个空字符串。
 
 
+        public String longestNiceSubstring(String s) {
+            int n = s.length();
+            int maxPos = 0;
+            int maxLen = 0;
+            for (int i = 0; i < n; ++i) {
+                int lower = 0;
+                int upper = 0;
+                for (int j = i; j < n; ++j) {
+                    if (Character.isLowerCase(s.charAt(j))) {
+                        lower |= 1 << (s.charAt(j) - 'a');
+                    } else {
+                        upper |= 1 << (s.charAt(j) - 'A');
+                    }
+                    if (lower == upper && j - i + 1 > maxLen) {
+                        maxPos = i;
+                        maxLen = j - i + 1;
+                    }
+                }
+            }
+            return s.substring(maxPos, maxPos + maxLen);
+        }
+
 
 }
