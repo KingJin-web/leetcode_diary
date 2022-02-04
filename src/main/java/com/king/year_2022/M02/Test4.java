f6235ef2c487502c5ac818ea6866bf05b0242e44
king <1261337209@qq.com>
2022-02-04 23:31:46 +0800

    ✔ 1725. 可以形成最大正方形的矩形数目

---
+++ src/main/java/com/king/year_2022/M02/Test4.java
@@ -0,0 +1,19 @@
+package com.king.year_2022.M02;
+
+/**
+ * @program: leetcode_diary
+ * @description: 1725. 可以形成最大正方形的矩形数目
+ * @author: King
+ * @create: 2022-02-04 23:31
+ */
+public class Test4 {
+    //给你一个数组 rectangles ，其中 rectangles[i] = [li, wi] 表示第 i 个矩形的长度为 li 、宽度为 wi 。
+    //
+    //如果存在 k 同时满足 k <= li 和 k <= wi ，就可以将第 i 个矩形切成边长为 k 的正方形。例如，矩形 [4,6] 可以切成边长最大为 4 的正方形。
+    //
+    //设 maxLen 为可以从矩形数组 rectangles 切分得到的 最大正方形 的边长。
+    //
+    //请你统计有多少个矩形能够切出边长为 maxLen 的正方形，并返回矩形 数目 。
+    //
+    //
+}
