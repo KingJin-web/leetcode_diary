05a0b772554b5e35ec0f44940171a8709a1e15b2
King <jinpeng.qmail@qq.com>
2022-02-09 23:38:49 +0800

    ✔ 2006. 差的绝对值为 K 的数对数目

---
+++ src/main/java/com/king/year_2022/M02/Test9.java
@@ -0,0 +1,10 @@
+package com.king.year_2022.M02;
+
+/**
+ * @program: leetcode_diary
+ * @description: 2006. 差的绝对值为 K 的数对数目
+ * @author: King
+ * @create: 2022-02-09 23:38
+ */
+public class Test9 {
+}
