dbf9189976edf14c2db5b52eaefeea36cfce1af4
King <jinpeng.qmail@qq.com>
2022-02-15 22:42:26 +0800

    ✔ 1380. 矩阵中的幸运数

--- src/main/java/com/king/year_2022/M02/Test14.java
+++ src/main/java/com/king/year_2022/M02/Test14.java
@@ -3,6 +3,9 @@ package com.king.year_2022.M02;
 import com.king.util.LeetcodeUtil;
 import com.king.util.MyPrint;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * @program: leetcode_diary
  * @description: 540. 有序数组中的单一元素
@@ -17,17 +20,36 @@ public class Test14 {
     //
     //你设计的解决方案必须满足 O(log n) 时间复杂度和 O(1) 空间复杂度。
 
-    //执行用时： 1 ms , 在所有 Java 提交中击败了 22.98% 的用户 内存消耗： 46.9 MB , 在所有 Java 提交中击败了 13.52% 的用户
-    public int singleNonDuplicate(int[] nums) {
+    //执行用时： 1 ms , 在所有 Java 提交中击败了 22.98% 的用户
+    // 内存消耗： 46.9 MB , 在所有 Java 提交中击败了 13.52% 的用户
+    public int singleNonDuplicate1(int[] nums) {
         int num = 0;
-        for(int i : nums)
+        for (int i : nums)
             num ^= i;
+
+        return num;
+    }
+
+    //执行用时： 1 ms , 在所有 Java 提交中击败了 22.98% 的用户
+    //内存消耗： 46.9 MB , 在所有 Java 提交中击败了 14.20% 的用户
+    public int singleNonDuplicate2(int[] nums) {
+        int num = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (i % 2 == 0){
+                num += nums[i];
+            }else {
+                num -= nums[i];
+            }
+
+        }
 
         return num;
     }
 
     public static void main(String[] args) {
         Test14 test14 = new Test14();
-        MyPrint.printObs(test14.singleNonDuplicate(LeetcodeUtil.stringToIntegerArray("[3,3,7,7,10,11,11]")));
+        MyPrint.printObs(test14.singleNonDuplicate1(LeetcodeUtil.stringToIntegerArray("[3,3,7,7,10,11,11]")));
+        MyPrint.printObs(test14.singleNonDuplicate2(LeetcodeUtil.stringToIntegerArray("[3,3,7,7,10,11,11]")));
+
     }
 }
---
+++ src/main/java/com/king/year_2022/M02/Test15.java
@@ -0,0 +1,10 @@
+package com.king.year_2022.M02;
+
+/**
+ * @program: leetcode_diary
+ * @description: 1380. 矩阵中的幸运数
+ * @author: King
+ * @create: 2022-02-15 22:41
+ */
+public class Test15 {
+}
