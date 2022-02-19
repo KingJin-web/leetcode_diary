c9313a54ce2509ea5bdb60fe5fda325364e1ebbe
King <jinpeng.qmail@qq.com>
2022-02-19 12:00:11 +0800

    ✔ 969. 煎饼排序

---
+++ src/main/java/com/king/year_2022/M02/Test19.java
@@ -0,0 +1,11 @@
+package com.king.year_2022.M02;
+
+/**
+ * @program: leetcode_diary
+ * @description: 969. 煎饼排序
+ * https://leetcode-cn.com/problems/pancake-sorting/
+ * @author: King
+ * @create: 2022-02-19 11:59
+ */
+public class Test19 {
+}
