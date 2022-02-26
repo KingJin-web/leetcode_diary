98ecb724a12c8040f0beb98d5cce5e80e8f61792
King <jinpeng.qmail@qq.com>
2022-02-26 23:27:59 +0800

    ✔ 258. 各位相加

---
+++ src/main/java/com/king/year_2022/M02/Test25.java
@@ -0,0 +1,11 @@
+package com.king.year_2022.M02;
+
+/**
+ * @program: leetcode_diary
+ * @description: 2016. 增量元素之间的最大差值
+ * https://leetcode-cn.com/problems/maximum-difference-between-increasing-elements/
+ * @author: King
+ * @create: 2022-02-26 21:55
+ */
+public class Test25 {
+}
