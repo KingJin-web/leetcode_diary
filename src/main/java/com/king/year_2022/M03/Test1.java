a165b6ef2eb78c8d93a47a79756031e2a9f7c8e7
King <jinpeng.qmail@qq.com>
2022-03-01 23:41:08 +0800

    ✔ 6. Z 字形变换

--- src/main/java/com/king/year_2022/M03/Test1.java
+++ src/main/java/com/king/year_2022/M03/Test1.java
@@ -1,5 +1,7 @@
 package com.king.year_2022.M03;
 
+import com.king.util.MyPrint;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -115,8 +117,37 @@ public class Test1 {
         }
         return target.toString();
     }
-    public static void main(String[] args) {
 
+    //将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+    //
+    //比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+    //
+    //P   A   H   N
+    //A P L S I I G
+    //Y   I   R
+    //之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+    //
+    //请你实现这个将字符串进行指定行数变换的函数：
+    //
+    //string convert(string s, int numRows);
+    //
+    //
+    //示例 1：
+    //
+    //输入：s = "PAYPALISHIRING", numRows = 3
+    //输出："PAHNAPLSIIGYIR"
+    //示例 2：
+    //输入：s = "PAYPALISHIRING", numRows = 4
+    //输出："PINALSIGYAHRPI"
+    //解释：
+    //P     I    N
+    //A   L S  I G
+    //Y A   H R
+    //P     I
+
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+        MyPrint.printObs(test1.convert("PAYPALISHIRING",4));
     }
 
 }
