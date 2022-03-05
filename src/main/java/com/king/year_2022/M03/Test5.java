96321de8fb9e9e65853de4073fe5a1432dd7cef5
King <jinpeng.qmail@qq.com>
2022-03-05 23:38:59 +0800

    ✔  521. 最长特殊序列 Ⅰ

---
+++ src/main/java/com/king/year_2022/M03/Test5.java
@@ -0,0 +1,16 @@
+package com.king.year_2022.M03;
+
+/**
+ * @program: leetcode_diary
+ * @description: 521. 最长特殊序列 Ⅰ
+ * @author: King
+ * @create: 2022-03-05 23:35
+ */
+public class Test5 {
+    public int findLUSlength(String a, String b) {
+        if(a.equals(b)){
+            return -1;
+        }
+        return a.length()>b.length()?a.length():b.length();
+    }
+}
