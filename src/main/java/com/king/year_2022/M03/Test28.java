f9fb86de0dcb9ba3ed9ea184e3fb9c45fd6e73f8
KingJin-web <1261337209@qq.com>
2022-03-28 21:27:35 +0800

    ✔ 693. 交替位二进制数

--- src/main/java/com/king/year_2022/M03/Test28.java
+++ src/main/java/com/king/year_2022/M03/Test28.java
@@ -1,5 +1,7 @@
 package com.king.year_2022.M03;
 
+import com.king.util.MyPrint;
+
 /**
  * @program: leetcode_diary
  * @description: 693. 交替位二进制数
@@ -34,5 +36,18 @@ public class Test28 {
         return (temp & (temp + 1)) == 0;
     }
 
+    //执行用时： 1 ms , 在所有 Java 提交中击败了 7.33% 的用户 内存消耗： 37.9 MB , 在所有 Java 提交中击败了 56.27% 的用户
+    public boolean hasAlternatingBits1(int n) {
+        String s = Integer.toBinaryString(n);
+        return !s.contains("11") && !s.contains("00");
+    }
 
+    public static void main(String[] args) {
+        Test28 test28 = new Test28();
+        MyPrint.print(test28.hasAlternatingBits(11));
+        MyPrint.print(test28.hasAlternatingBits1(11));
+        MyPrint.print(test28.hasAlternatingBits(5));
+        MyPrint.print(test28.hasAlternatingBits1(5));
+
+    }
 }
