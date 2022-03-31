42be1e75940dd00b8d525325fcdc4a39494e7b16
KingJin-web <1261337209@qq.com>
2022-03-31 23:44:42 +0800

    ✔ 728. 自除数

--- src/main/java/com/king/year_2022/M03/Test31.java
+++ src/main/java/com/king/year_2022/M03/Test31.java
@@ -5,16 +5,25 @@ import java.util.List;
 
 /**
  * @program: leetcode_diary
- * @description:
+ * @description: 728. 自除数
+ * https://leetcode-cn.com/problems/self-dividing-numbers/
  * @author: King
  * @create: 2022-03-31 23:40
  */
 public class Test31 {
+
+    //自除数 是指可以被它包含的每一位数整除的数。
+    //
+    //例如，128 是一个 自除数 ，因为 128 % 1 == 0，128 % 2 == 0，128 % 8 == 0。
+    //自除数 不允许包含 0 。
+    //
+    //给定两个整数 left 和 right ，返回一个列表，列表的元素是范围 [left, right] 内所有的 自除数 。
+
+    //执行用时： 1 ms , 在所有 Java 提交中击败了 98.33% 的用户 内存消耗： 38.9 MB , 在所有 Java 提交中击败了 45.13% 的用户
     public List<Integer> selfDividingNumbers(int left, int right) {
         List<Integer> res = new ArrayList<>();
         for (int i = left; i <= right; i++) {
-            int curNum = i;
-            if (check(curNum)) res.add(curNum);
+            if (check(i)) res.add(i);
         }
         return res;
     }
