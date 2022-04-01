786025f2fd02bd6dc313b1036eb20b6d1929de6b
KingJin-web <1261337209@qq.com>
2022-04-02 02:31:22 +0800

    ✔ 28. 实现 strStr()

--- src/main/java/com/king/year_2022/M04/Test2.java
+++ src/main/java/com/king/year_2022/M04/Test2.java
@@ -53,4 +53,47 @@ public class Test2 {
         System.out.println(test2.isMonotonic(nums4));
 
     }
+
+    /**
+     * 28. 实现 strStr()
+     */
+    private static class Test1_2 {
+
+        //执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户 内存消耗： 39.3 MB , 在所有 Java 提交中击败了 71.60% 的用户
+        public int strStr(String haystack, String needle) {
+            char[] haystackChars = haystack.toCharArray();
+            char[] needleChars = needle.toCharArray();
+            int haystackLength = haystackChars.length;
+            int needleLength = needleChars.length;
+            if (needleLength == 0) {
+                return 0;
+            }
+            if (haystackLength < needleLength) {
+                return -1;
+            }
+            for (int i = 0; i < haystackLength - needleLength + 1; i++) {
+                if (haystackChars[i] == needleChars[0]) {
+                    boolean isEqual = true;
+                    for (int j = 1; j < needleLength; j++) {
+                        if (haystackChars[i + j] != needleChars[j]) {
+                            isEqual = false;
+                            break;
+                        }
+                    }
+                    if (isEqual) {
+                        return i;
+                    }
+                }
+
+            }
+            return -1;
+        }
+
+        public static void main(String[] args) {
+            Test1_2 test1_2 = new Test1_2();
+            System.out.println(test1_2.strStr("hello", "ll"));
+            System.out.println(test1_2.strStr("aaaaa", "bba"));
+            System.out.println(test1_2.strStr("mississippi", "issip"));
+        }
+    }
 }
