90c27c4c5a1e2b7f63152c40f9aa583a8824d758
KingJin-web <1261337209@qq.com>
2022-04-10 23:56:55 +0800

    ✔ 804. 唯一摩尔斯密码词

---
+++ src/main/java/com/king/year_2022/M04/Test10.java
@@ -0,0 +1,54 @@
+package com.king.year_2022.M04;
+
+import java.util.HashSet;
+
+/**
+ * @program: leetcode_diary
+ * @description: 804. 唯一摩尔斯密码词
+ * @author: King
+ * @create: 2022-04-10 23:51
+ */
+public class Test10 {
+    private static String[] map = {
+            ".-",
+            "-...",
+            "-.-.",
+            "-..",
+            ".",
+            "..-.",
+            "--.",
+            "....",
+            "..",
+            ".---",
+            "-.-",
+            ".-..",
+            "--",
+            "-.",
+            "---",
+            ".--.",
+            "--.-",
+            ".-.",
+            "...",
+            "-",
+            "..-",
+            "...-",
+            ".--",
+            "-..-",
+            "-.--",
+            "--.."
+    };
+
+    public int uniqueMorseRepresentations(String[] words) {
+        if (words == null) return 0;
+        HashSet<String> set = new HashSet<String>();
+        for (String s : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : s.toCharArray()) {
+                sb.append(map[c - 'a']);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+
+}
