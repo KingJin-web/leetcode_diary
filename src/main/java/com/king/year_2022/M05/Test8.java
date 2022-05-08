d605c6270cdf2563d10f223f6f2e38ee064a13ab
KingJin-web <1261337209@qq.com>
2022-05-09 00:03:17 +0800

    ✔ 442. 数组中重复的数据

--- src/main/java/com/king/year_2022/M05/Test8.java
+++ src/main/java/com/king/year_2022/M05/Test8.java
@@ -1,7 +1,9 @@
 package com.king.year_2022.M05;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author: 12613
@@ -15,13 +17,31 @@ public class Test8 {
         List<Integer> ret = new ArrayList<>();
 
         int n = nums.length;
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             nums[(nums[i] - 1) % n] += n;
         }
 
-        for(int i = 0; i < n; i++){
-            if(nums[i] > 2 * n) ret.add(i+1);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 2 * n) ret.add(i + 1);
         }
         return ret;
+    }
+
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ret = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                ret.add(num);
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Test8 test8 = new Test8();
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        System.out.println(test8.findDuplicates(nums));
+        System.out.println(test8.findDuplicates1(nums));
     }
 }
