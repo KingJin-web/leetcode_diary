1c8204b7a6e33cb174e5fff893cecdd43e64a6cd
KingJin-web <1261337209@qq.com>
2022-06-09 23:49:55 +0800

    ✔ 497. 非重叠矩形中的随机点

---
+++ src/main/java/com/king/year_2022/M06/Test9.java
@@ -0,0 +1,11 @@
+package com.king.year_2022.M06;
+
+/**
+ * @author: 12613
+ * @project: leetcode_diary
+ * @pcakage: com.king.year_2022.M06.Test9
+ * @date: 2022年06月09日 23:35
+ * @description: ${}
+ */
+public class Test9 {
+}
