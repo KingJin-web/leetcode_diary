794e98e4c2dab2f8c8b28c4b72cb74d6239fb1c3
KingJin-web <1261337209@qq.com>
2022-06-12 23:50:59 +0800

    ✔ 890. 查找和替换模式

---
+++ src/main/java/com/king/year_2022/M06/Test12.java
@@ -0,0 +1,37 @@
+package com.king.year_2022.M06;
+
+/**
+ * @author: 12613
+ * @project: leetcode_diary
+ * @pcakage: com.king.year_2022.M06.Test12
+ * @date: 2022年06月12日 23:49
+ * @description: ${}
+ */
+public class Test12 {
+
+    class Solution {
+        public List<String> findAndReplacePattern(String[] words, String pattern) {
+            List<String> ans = new ArrayList<String>();
+            for (String word : words) {
+                if (match(word, pattern) && match(pattern, word)) {
+                    ans.add(word);
+                }
+            }
+            return ans;
+        }
+
+        public boolean match(String word, String pattern) {
+            Map<Character, Character> map = new HashMap<Character, Character>();
+            for (int i = 0; i < word.length(); ++i) {
+                char x = word.charAt(i), y = pattern.charAt(i);
+                if (!map.containsKey(x)) {
+                    map.put(x, y);
+                } else if (map.get(x) != y) { // word 中的同一字母必须映射到 pattern 中的同一字母上
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}
