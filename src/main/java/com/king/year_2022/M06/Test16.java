3e9423811ec62b2f8526126ff6d755a62f7300b4
KingJin-web <1261337209@qq.com>
2022-06-16 22:55:29 +0530

    ✔ 532. 数组中的 k-diff 数对

--- src/main/java/com/king/year_2022/M06/Test16.java
+++ src/main/java/com/king/year_2022/M06/Test16.java
@@ -1,5 +1,8 @@
 package com.king.year_2022.M06;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: 12613
  * @project: leetcode_diary
@@ -12,9 +15,9 @@ public class Test16 {
         Map<Integer,Integer> map=new HashMap<>();
         int count=0;
         if(k<0)return count;
-        for(int i=0;i<nums.length;i++){
-            map.putIfAbsent(nums[i],0);
-            map.put(nums[i],map.get(nums[i])+1);
+        for (int num : nums) {
+            map.putIfAbsent(num, 0);
+            map.put(num, map.get(num) + 1);
         }
         for(int i:map.keySet()){
             if(k==0){
