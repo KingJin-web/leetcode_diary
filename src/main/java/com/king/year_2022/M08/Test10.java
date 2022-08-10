bb74e3384633a2b19368328af289ae873abf561f
KingJin-web <1261337209@qq.com>
2022-08-10 23:56:16 +0800

    ✔ 640. 求解方程

---
+++ src/main/java/com/king/year_2022/M08/Test10.java
@@ -0,0 +1,12 @@
+package com.king.year_2022.M08;
+
+/**
+ * @author: King
+ * @project: leetcode_diary
+ * @date: 2022年08月10日 23:42
+ * @description: 640. 求解方程
+ */
+public class Test10 {
+
+
+}
---
+++ src/main/java/com/king/year_2022/M08/Test9.java
@@ -0,0 +1,10 @@
+package com.king.year_2022.M08;
+
+/**
+ * @author: King
+ * @project: leetcode_diary
+ * @date: 2022年08月09日 22:39
+ * @description: 1413. 逐步求和得到正数的最小值
+ */
+public class Test9 {
+}
