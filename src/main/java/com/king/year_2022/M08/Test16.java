3d2b4d5f0da0843c9fc4f311e5d74ad9ba9bbd13
KingJin-web <1261337209@qq.com>
2022-08-16 23:58:53 +0800

    ✔ 1656. 设计有序流

---
+++ src/main/java/com/king/year_2022/M08/Test16.java
@@ -0,0 +1,20 @@
+package com.king.year_2022.M08;
+
+/**
+ * @author: King
+ * @project: leetcode_diary
+ * @date: 2022年08月16日 23:58
+ * @description:
+ */
+public class Test16 {
+    class OrderedStream {
+
+        public OrderedStream(int n) {
+
+        }
+
+        public List<String> insert(int idKey, String value) {
+
+        }
+    }
+}
